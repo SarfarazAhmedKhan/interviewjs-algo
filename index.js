8c45e22a34823dc4e6e41f30e64b1b15d8851a3a
sarfaraz.khan@uniquesoftwaredev.com <sarfaraz.khan@uniquesoftwaredev.com>
2022-03-02 17:17:42 +0500

    added first two smallest sort

--- index.js
+++ index.js
@@ -249,3 +249,44 @@ function removeduplicateValues(array) {
 //   "removeDuplicateValues",
 //   removeduplicateValues([1, 2, 3, 2, 4, 6, 7, 4, 5, 8, 8])
 // );
+function print2Smallest( arr, arr_size)
+{
+    let i, first, second;
+
+    /* There should be atleast two elements */
+    if (arr_size < 2)
+    {
+        document.write(" Invalid Input ");
+        return;
+    }
+
+    first=Number.MAX_VALUE ;
+    second=Number.MAX_VALUE ;
+    for (i = 0; i < arr_size ; i ++)
+    {
+        /* If current element is smaller than first
+        then update both first and second */
+        if (arr[i] < first)
+        {
+            second = first;
+            first = arr[i];
+        }
+
+        /* If arr[i] is in between first and second
+        then update second */
+        else if (arr[i] < second && arr[i] != first)
+            second = arr[i];
+    }
+    if (second == Number.MAX_VALUE )
+        document.write("There is no second smallest element\n");
+    else
+        document.write("The smallest element is " + first + " and second "+
+            "Smallest element is " + second +'\n');
+}
+
+
+    // Driver program
+
+    // let arr = [ 12, 13, 1, 10, 34, 1 ];
+    // let n = arr.length;
+    // print2Smallest(arr, n);
